1d16507cb31f9361bf28e3dc1521cbebbc4040ad
Gabriel Alves Carvalho <gabriel_alvescarvalho@hotmail.com>
2023-11-12 14:15:35 -0300

    Continuando o Feed de Instituicao

--- ConecteDoe.WebApp/Controllers/PlataformaController.cs
+++ ConecteDoe.WebApp/Controllers/PlataformaController.cs
@@ -1,19 +1,43 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using ConecteDoe.Dados;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ConecteDoe.WebApp.Controllers
 {
     public class PlataformaController : Controller
     {
+        private Contexto db = new Contexto();
+
         [Authorize]
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult FeedInstituicao()
+        //public IActionResult FeedInstituicao()
+        //{
+            
+
+        //    return View();
+        //}
+
+        public IActionResult FeedInstituicao(int pagina = 1, int itensPorPagina = 10)
         {
-            return View();
+            int indiceInicial = (pagina - 1) * itensPorPagina;
+
+            var resultadoPaginado = db.Usuario
+                .OrderBy(u => u.Nome)
+                .Skip(indiceInicial)
+                .Take(itensPorPagina)
+                .ToList();
+
+            ViewBag.PaginaAtual = pagina;
+            ViewBag.TotalItens = db.Usuario.Count();
+            ViewBag.ItensPorPagina = itensPorPagina;
+            ViewBag.TotalPaginas = (int)Math.Ceiling((double)ViewBag.TotalItens / ViewBag.ItensPorPagina);
+
+            return View(resultadoPaginado);
         }
+
     }
 }
---
+++ ConecteDoe.WebApp/Models/CardInstituicaoViewModel.cs
@@ -0,0 +1,10 @@
+﻿using ConecteDoe.Dominio.Entities;
+
+namespace ConecteDoe.WebApp.Models
+{
+    public class CardInstituicaoViewModel
+    {
+        public Instituicao instituicao { get; set; }
+        public DadosInstituicao dadosInstituicao { get; set; }
+    }
+}
